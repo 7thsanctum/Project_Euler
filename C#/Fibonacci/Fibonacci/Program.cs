108696e0a20b69bd1d37a6c1a8276dc1b38b756d
Mark M. Miller <markmmiller@hotmail.co.uk>
2013-08-01 22:34:01 +0100

    Initial Commit
    This is the first commit for the Project Euler Project.

---
+++ C#/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static long total = 0;
+        static long limit = 4000000;
+        static void Main(string[] args)
+        {
+            nextFib(1, 2);
+            Console.Write("\nDONE : " + total + 2); // Add 2 because the nextFib skips the first 2 that's entered
+            Console.ReadKey();
+        }
+
+        static void nextFib(long x, long y)
+        {
+            long currX = y;
+            long currY = x + y;
+            
+            Console.Write("\n" + y);
+            if (currY > limit) return;
+            else
+            {
+                if (currY % 2 == 0) total += currY;
+                nextFib(currX, currY);
+            }
+        }
+    }
+}
---
+++ C#/Prime Numbers/Prime Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static long limit = 600851475143;
+        static bool[] numberList = new bool[limit + 1];
+
+        static void Main(string[] args)
+        {
+            long total = 0;
+            for (int i = 2; i <= limit; i++)
+            {
+                if (!numberList[i])
+                {
+                    //total += i;
+                    for (int k = 2 * i; k <= limit; k += i)
+                    {
+                        numberList[k] = true;
+                    }
+                }
+            }
+            
+            Console.Write("Total : " + total);
+            Console.ReadKey();
+        }
+
+    }
+}
