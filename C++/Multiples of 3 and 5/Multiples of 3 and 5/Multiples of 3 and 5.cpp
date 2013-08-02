b1166f10c4acf3258c305bbdc94f54ce381c20ce
unknown <10006179@ME0B056-01205B.c27-merch.napier.ac.uk>
2013-08-02 14:35:28 +0100

    Added problem 1 for C++

---
+++ C++/Multiples of 3 and 5/Multiples of 3 and 5/Multiples of 3 and 5.cpp
@@ -0,0 +1,18 @@
+// Multiples of 3 and 5.cpp : Defines the entry point for the console application.
+//
+# include "stdafx.h"
+# include <iostream>
+
+using  namespace std;
+
+int main()
+{
+	int total = 0;
+    for (int i = 0; i < 1000; i++) 
+		if (i % 3 == 0 || i % 5 == 0) total += i;
+
+	cout << "Total : " << total << "\n";
+	
+	return 0;
+}
+
