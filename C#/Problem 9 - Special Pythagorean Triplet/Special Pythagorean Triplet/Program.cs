e7fbab65af9138b7af2a1150e96a26995e1375ac
unknown <10006179@ME0B056-01205B.c27-merch.napier.ac.uk>
2013-08-02 11:42:13 +0100

    Completed Problem 9 - Special Pythagorean Triplet

---
+++ C#/Problem 9 - Special Pythagorean Triplet/Special Pythagorean Triplet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Special_Pythagorean_Triplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 1000;
+            int foundX = 0;
+            int foundY = 0;
+            for (int x = 0; x < goal; x++)
+            {
+                for (int y = 0; y < goal; y++)
+                {
+                    if (x + y + (Math.Sqrt((x * x) + (y * y))) == 1000 && x != 0 && y != 0 )
+                    {
+                        foundX = x;
+                        foundY = y;
+                        Console.Write("\nFound X : " + x);
+                        Console.Write("\nFound Y : " + y);
+                        Console.Write("\nFound C : " + Math.Sqrt((x * x) + (y * y)));
+                        Console.Write("\nFound Root : " + (x * x) + (y * y));                        
+                        Console.ReadKey();
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
